3fe1afedbb2540c7478a1c5d450bfcbc4552c15c
wduf <wduf02@gmail.com>
2024-08-19 22:34:21 -0400

    updated hero top spacing

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <>
       <Section id='hero'>
-        <div className='max-w-lg mx-auto'>
+        <div className='max-w-lg mx-auto pt-4'>
           <h1 className='text-3xl font-semibold pb-2'>Hi, I'm Will!</h1>
           <p>I'm a software engineer interested in full-stack development and cloud computing. I work as a Software Engineer at BJ's Wholesale Club and as a freelance Web Developer on Upwork.</p>
         </div>
